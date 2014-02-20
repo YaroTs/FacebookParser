cc04251e814d364e2637b01784c77796738c1ff1
YaroTs <yaroslav.tsimokha@gmail.com>
2014-02-20 17:31:05 +0400

    test data

---
+++ testdata/test_about_files/1hcBWLY0Lmh.js
@@ -0,0 +1,5 @@
+/*!CK:3502484623!*//*1386558162,173217053*/
+
+if (self.CavalryLogger) { CavalryLogger.start_js(["Bt8C\/"]); }
+
+__d("XNotificationInlineStoryControllerURIBuilder",["XControllerURIBuilder"],function(a,b,c,d,e,f){var g=b('XControllerURIBuilder');e.exports=g.create("\/desktop_notifications\/inline_story\/",{notif_id:{type:"String",required:true},flyout_id:{type:"String",required:true}});});
